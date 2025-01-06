36fb42f144201e23c1a1ae14e90ad02a0a833728
Artur Yandulsky <artur.yandulsky@jetruby.com>
2025-01-06 14:32:54 +0300

    Updated login screen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,9 +16,13 @@ interface SignOutButtonProps {
 
 function SignInButton({ onSignIn }: SignInButtonProps) {
   return (
-    <button onClick={onSignIn} className="group bg-blue-50 px-3 py-1 rounded flex justify-between items-center gap-x-3">
-      <span className="text-blue-600 group-hover:text-blue-700 text-xs font-semibold">Sign In</span>
-      <img src={GoogleLogo} alt="sign in with google" width={24} height={24} />
+    <button onClick={onSignIn} className="group flex justify-between items-stretch">
+      <div className="grow flex items-center bg-blue-600 rounded-l-xl border-4 border-blue-600  px-6">
+        <span className="text-start text-2xl font-bold text-stone-50">Sign In</span>
+      </div>
+      <div className="w-16 group-hover:w-1/2 duration-500 ease-out delay-200 bg-white py-3 border-4 border-blue-600 rounded-r-xl">
+        <img src={GoogleLogo} alt="sign in with google" width={36} height={36} className="mx-auto" />
+      </div>
     </button>
   )
 }
@@ -41,25 +45,41 @@ function SignOutButton({ onSignOut, user }: SignOutButtonProps) {
   )
 }
 
-export function Layout() {
-  const [user] = useAuthState(auth)
-
+function NonAuthorizedUser() {
   const googleSignIn = async () => {
     await signInWithPopup(auth, new GoogleAuthProvider())
   }
+
+  return (
+    <main className="h-screen flex justify-center items-center">
+      <div className="flex flex-col gap-y-5 w-80">
+        <h2 className="text-xl font-semibold text-zinc-700 text-center">You are not authorized, please sign in!</h2>
+        <SignInButton onSignIn={googleSignIn} />
+      </div>
+    </main>
+  )
+}
+
+export function Layout() {
+  const [user, isChecking] = useAuthState(auth)
+
   const signOut = () => {
     auth.signOut()
   }
+
+  if (isChecking) return <div />
+
+  if (!user) return <NonAuthorizedUser />
 
   return (
     <div className="min-h-screen w-full bg-blue-50 relative">
       <header className="sticky top-0 z-50 bg-stone-700 px-12 py-5 flex items-center justify-between h-[5.25rem]">
         <h1 className="text-xl text-blue-50 font-medium">Chat test</h1>
-        {user ? <SignOutButton onSignOut={signOut} user={user} /> : <SignInButton onSignIn={googleSignIn} />}
+        <SignOutButton onSignOut={signOut} user={user} />
       </header>
       <div className="flex">
         <main className="grow"> Main Part</main>
-        <section className="w-96 h-[calc(100vh-5.25rem)] flex flex-col p-5 justify-between bg-white border-l border-blue-100">
+        <section className="w-96 h-[calc(100dvh-5.25rem)] flex flex-col p-5 justify-between bg-white border-l border-blue-100">
           {user ? (
             <>
               <ChatBox />
